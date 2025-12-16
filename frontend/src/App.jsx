751d2dcfe9947e58c4d27f0138f6a02be9702b64
nimra <snimramukhtar321@gmail.com>
2025-12-16 21:06:43 +0500

    removed nav from signup/signin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Services from './pages/Services';
@@ -11,31 +11,41 @@ import BundleDetails from './pages/BundlesDetails';
 import ScrollToTop from './components/ScrollToTop';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+
 function App() {
   return (
     <CartProvider>
       <Router>
         <ScrollToTop />
-        <div className="min-h-screen">
-          <Navbar />
-          <div className='mt-[65px]'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/services/:id" element={<ServicesDetails />} />
-
-            <Route path="/bundles" element={<Bundles />} />
-            <Route path="/bundles/:id" element={<BundleDetails />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path='/signin' element={<Signin />} />
-    <Route path='/signup' element={<Signup />} />
-
-          </Routes>
-          </div>
-        </div>
+        <Layout />
       </Router>
     </CartProvider>
+  );
+}
+
+function Layout() {
+  const location = useLocation();
+  
+  const noNavbarRoutes = ['/signin', '/signup'];
+  const showNavbar = !noNavbarRoutes.includes(location.pathname);
+
+  return (
+    <div className="min-h-screen">
+      {showNavbar && <Navbar />}
+      <div className={showNavbar ? 'mt-[65px]' : ''}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/:id" element={<ServicesDetails />} />
+          <Route path="/bundles" element={<Bundles />} />
+          <Route path="/bundles/:id" element={<BundleDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
