19799efed84e8f9021a20936a61de6a0249d7264
bilal1319 <bilalkhsherwani1319@gmail.com>
2025-12-18 21:09:56 +0500

    resolved conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,13 +14,9 @@ import BundleDetails from './pages/BundlesDetails';
 import ScrollToTop from './components/ScrollToTop';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
-<<<<<<< HEAD
 import AdminBoard from './pages/AdminBoard'
 import ServicesManagement from './pages/ServicesManagement';
 import BundlesManagement from './pages/BundlesManagement';import AdminLogin from './pages/AdminLogin';
-=======
-import AdminLogin from './pages/AdminLogin';
->>>>>>> origin/faizan-backend
 import AdminSignup from './pages/AdminSignup';
 import AdminDashboard from './pages/AdminDashboard';
 import useAuthStore from './store/authStore';
@@ -108,13 +104,10 @@ function Layout() {
           <Route path="/checkout/cancel" element={<CheckoutCancel />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
-<<<<<<< HEAD
            <Route path="/admin/service" element={<ServicesManagement />} />
     <Route path="/admin/bundle" element={<BundlesManagement />} />
       <Route path="/admin" element={<AdminBoard />}/>
 
-=======
->>>>>>> origin/faizan-backend
           <Route path='/admin/login' element={<AdminLogin />} />
           <Route path='/admin/signup' element={<AdminSignup />} />
           <Route path='/admin/dashboard' element={<AdminDashboard />} />
