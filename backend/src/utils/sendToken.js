91d77620195944278207a3d3ace834843aa18768
bilal1319 <bilalkhsherwani1319@gmail.com>
2025-12-20 21:38:26 +0500

    cors fix

--- backend/src/utils/sendToken.js
+++ backend/src/utils/sendToken.js
@@ -10,7 +10,7 @@ export const sendToken = (user, statusCode, res) => {
   const options = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
   };
 
